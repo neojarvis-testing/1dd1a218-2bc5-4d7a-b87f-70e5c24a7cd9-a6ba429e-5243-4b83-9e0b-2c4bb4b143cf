1f54fdbeae194607d9a30e4f4896fcabdd5cb136
huntritik <tiwariritik95@gmail.com>
2025-04-14 14:10:57 +0000

    Final commit

---
+++ Project/features/feature2.feature
@@ -0,0 +1,18 @@
+Feature: Homedepot Automation Functionality
+ 
+    Scenario Outline:PHDTC-01 Verify the Wrench and  Explore
+ 
+    Given User clicks on searchbox and enter the data "<Product>"
+ 
+    When User selects category and verify
+ 
+    Then User explore different wrenches
+    Examples:
+        | Product |
+        | wrench  |
+ 
+ 
+    Scenario:PHDTC-07 Verify and explore About Us section
+ 
+    Given User Scrolls to footer
+    Then  User Clicks every Link and Verify Information    
---
+++ Project/src/test/java/stepdefinition/stepdefinition1.java
@@ -0,0 +1,54 @@
+package stepdefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import pages.HomepageActionPage;
+
+public class stepdefinition1 {
+
+    HomepageActionPage act = new HomepageActionPage();
+
+
+    /**
+     * Decription:This method is to print the user current information
+     * @author 10830767 -Raghul Senthil
+     * Test Case ID: PHDTC-005
+     */
+    @Given("User lands on the homepage")
+    public void user_lands_on_the_homepage() {
+        System.out.println("Browser started");
+
+    }
+
+     /**
+     * Decription:This method is to iterate through the footers  under the support section
+     * @author 10830767 -Raghul Senthil
+     * Test Case ID: PHDTC-005
+     */
+    @Then("User clicks on every footer link and navigate back")
+    public void user_clicks_on_every_footer_link_and_navigate_back() {
+        act.footerTestcase01();
+
+    }
+
+    /**
+     * Decription:This method is to print the user current information
+     * @author 10830767 -Raghul Senthil
+     * Test Case ID: PHDTC-006
+     */
+    @Given("User land on the homepage")
+    public void user_land_on_the_homepage() {
+        System.out.println("Browser started");
+    }
+
+     /**
+     * Decription:This method is to iterate through the footers  under the resources link session
+     * @author 10830767 -Raghul Senthil
+     * Test Case ID: PHDTC-006
+     */
+    @Then("User clicks on the footer link under resouces and assertion")
+    public void user_clicks_on_the_footer_link_under_resouces_and_assertion() {
+        act.footerTestcase02();
+    }
+
+}
